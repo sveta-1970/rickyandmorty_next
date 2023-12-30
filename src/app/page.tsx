f4e324a46fc47fdc665dc786117014df09a12120
sveta-1970 <ssarisoydan@gmail.com>
2023-12-30 03:15:02 +0200

    Created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,25 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>Main page</div>
       <div className={styles.banner}>
         
         <Image
           src="/rick-and-morty2_1.png"
           alt="Logo"
           className={styles.logo}
-          width={300}
-          height={200}
+          width={270}
+          height={210}
           priority
         />
       </div>
 
-      <div className={styles.grid}>
+      <div className={styles.footer}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="https://rickandmortyapi.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Find in-depth information about Rick and Morty API.</p>
         </a>
       </div>
     </main>
