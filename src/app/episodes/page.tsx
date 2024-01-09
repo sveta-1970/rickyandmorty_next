6cbca54cc00959a809baa252f0caf4a1b19a6ec1
sveta-1970 <ssarisoydan@gmail.com>
2024-01-09 20:24:56 +0200

    Added pagination to episodes page

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -1,5 +1,5 @@
 //"episodes": "https://rickandmortyapi.com/api/episode"
-
+/*
 import { req } from "@/components/services/server.request";
 import Cards from "@/components/Cards/index";
 import { IFetchResult } from "@/components/interfaces/interface";
@@ -19,4 +19,80 @@ export default async function fetchEpisodes() {
   } catch (e) {
     console.error(e);
   }
+}*/
+
+"use client";
+
+import { useState, useEffect } from "react";
+import { req } from "@/components/services/server.request";
+import Pagination from "@/components/Pagination/Pagination";
+import Cards from "@/components/Cards/index";
+import { ICharacter, IEpisode, ILocation } from "@/components/interfaces/interface";
+
+export default function fetchEpisodes() {
+  type NextPageUrl = string;
+  type currentPageUrl = string;
+  type PrevPageUrl = string;
+  type Pages = number;
+  type episodes = (ICharacter | ILocation | IEpisode)[];
+  type loading = boolean;
+
+  const [loading, setLoading] = useState(true);
+  const [episodes, setEpisodes] = useState<
+    (ICharacter | ILocation | IEpisode)[]
+  >([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://rickandmortyapi.com/api/episode"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState<NextPageUrl>();
+  const [prevPageUrl, setPrevPageUrl] = useState<PrevPageUrl>();
+  const [pages, setPages] = useState<Pages>();
+
+  useEffect(() => {
+    const url = currentPageUrl;
+    setLoading(true);
+    const fetchData = async () => {
+      const data = await req(url);
+      setEpisodes(data.results);
+      setLoading(false);
+      setNextPageUrl(data.info.next);
+      setPrevPageUrl(data.info.prev);
+      setPages(data.info.pages);
+    };
+    fetchData();
+  }, [currentPageUrl]);
+
+  function nextPage() {
+    if (nextPageUrl) {
+      setCurrentPageUrl(nextPageUrl);
+    }
+  }
+
+  function prevPage() {
+    if (prevPageUrl) {
+      setCurrentPageUrl(prevPageUrl);
+    }
+  }
+
+  function goToPage(num: number) {
+    setCurrentPageUrl(`https://rickandmortyapi.com/api/episode?page=${num}`);
+  }
+
+  return (
+    <>
+      {loading && <div>Loading...</div>}
+      {!loading && (
+        <main>
+          <h1>Episodes</h1>
+          <Cards data={episodes} />
+          <Pagination
+            nextPage={nextPageUrl ? nextPage : null}
+            prevPage={prevPageUrl ? prevPage : null}
+            goToPage={goToPage}
+            pages={pages}
+          />
+        </main>
+      )}
+    </>
+  );
 }
