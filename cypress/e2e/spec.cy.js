b2a293667b3f44e2df7baad9d69520f27e260997
sveta-1970 <ssarisoydan@gmail.com>
2024-02-07 00:15:39 +0200

    Updated cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,17 @@
 describe("Select test", () =>{
-  it("Should open the website and perform selecion of all categories", () => {
+  it("Should open the website and perform selection of all categories", () => {
     cy.visit("https://rickyandmorty-next-jsua.vercel.app");
 
     //Click button with id "characters"
     cy.get("#characters").click();
 
     //Verify that selected results are all shown on the page and they are correct
-    cy.get(".styles.card").each(($card) => {
+    cy.get(".card").each(($card) => {
       cy.wrap($card).within(() => {
-        cy.get(".styles.name").should("exist");
-        cy.get(".styles.image").should("exist");
-        cy.get(".styles.description").should("exist");
-        cy.get(".styles.description").within(() => {
+        cy.get(".name").should("exist");
+        cy.get(".image").should("exist");
+        cy.get(".description").should("exist");
+        cy.get(".description").within(() => {
           cy.get("p").eq(0).should("contain", "Status: ");
           cy.get("p").eq(1).should("contain", "Species: ");
           cy.get("p").eq(2).should("contain", "Type: ");
